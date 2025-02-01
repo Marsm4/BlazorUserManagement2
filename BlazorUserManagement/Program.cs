fa2bf81594abbab9a11730fec28afecec68285f0
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2025-02-01 22:25:40 +0300

    123

--- BlazorUserManagement/Program.cs
+++ BlazorUserManagement/Program.cs
@@ -3,15 +3,10 @@ using BlazorUserManagement.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-
-
-
-
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7253/api/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44357/api/") }); // исправленный URL
 
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<UserService>();
